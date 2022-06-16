c8be34a2b67f936c592d764626c803d4dcf72522
Ansaar <ansaar.ahmed245@gmail.com>
2022-06-15 19:07:38 -0700

    evaluation part 1 and 2

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,6 +1,6 @@
 export const DOTS = "...";
 
-function usePagination() {
+function usePagination({ currentPage, totalCount, pageSize }) {
   /*
     Rewrite the logic here to map out the pagination to be displayed
 
@@ -8,7 +8,45 @@ function usePagination() {
     Please replace this comment here with a description of this hook.
     
   */
-  return [1, 2, 3, DOTS, 5];
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(totalCount / pageSize);
+  // empty array to store the pagination
+  let pages = [];
+
+  // If there is only one page, display 1
+  if (totalPages <= 1) {
+    return [1];
+  }
+  // 1, 2, 3 ...13
+  if (currentPage <= 2) {
+    for (let i = 1; i <= 3; i++) {
+      pages.push(i);
+    }
+    pages.push(DOTS);
+    pages.push(totalPages);
+    return pages;
+
+    // 1 ...11, 12, 13
+  } else if (currentPage >= totalPages - 1) {
+    pages.push(1);
+    pages.push(DOTS);
+    for (let i = totalPages - 2; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
+
+  // 1 ... 2, 3, 4 ...13
+  else {
+    pages.push(1);
+    pages.push(DOTS);
+    for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+      pages.push(i);
+    }
+    pages.push(DOTS);
+    pages.push(totalPages);
+    return pages;
+  }
 }
 
 export default usePagination;
